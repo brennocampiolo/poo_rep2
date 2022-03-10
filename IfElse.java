1c7273a64666bce751c0257eb6cedd68fef6a7cb
Brenno Campiolo <aluno@IPIRANGA.FATEC>
2022-03-10 16:27:14 -0300

    Exemplos de IfElse

--- IfElse.java
+++ IfElse.java
@@ -5,7 +5,7 @@ public class IfElse {
         Scanner leitor = new Scanner (System.in);
         System.out.printf("Digite a sua nota\n");
         int nota = leitor.nextInt();
-        if ( nota >=6);
+        if ( nota >=6)
            System.out.printf ("Parabens");
            System.out.printf(
                "A nota %d lhe garante aprovação\n",
